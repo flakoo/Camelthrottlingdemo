38ccad47a07ccef9521320fe6bf82214da6159f0
Jan Falkowski <falkowski.jj@gmail.com>
2016-07-13 22:42:50 +0200

    Increased number to 4 connectors 2 msgs per second each

--- src/main/java/org/alpacology/ApplicationConfiguration.java
+++ src/main/java/org/alpacology/ApplicationConfiguration.java
@@ -6,7 +6,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApplicationConfiguration {
     @Bean(name = "kacperConnector")
-    public SmsConnector kacperConnector() {
+        public SmsConnector kacperConnector() {
+            return new SmsConnector();
+    }
+
+    @Bean(name = "tomekConnector")
+    public SmsConnector tomekConnector() {
+        return new SmsConnector();
+    }
+
+    @Bean(name = "przemoConnector")
+    public SmsConnector przemoConnector() {
+        return new SmsConnector();
+    }
+
+    @Bean(name = "wojtekConnector")
+    public SmsConnector wojtekConnector() {
         return new SmsConnector();
     }
 }
--- src/main/java/org/alpacology/camel/SmsRouter.java
+++ src/main/java/org/alpacology/camel/SmsRouter.java
@@ -11,5 +11,17 @@ public class SmsRouter extends RouteBuilder {
                 .throttle(2)
                 .timePeriodMillis(1000)
                 .bean("kacperConnector", "send(${body}");
+        from("jms:queue:sms")
+                .throttle(2)
+                .timePeriodMillis(1000)
+                .bean("tomekConnector", "send(${body}");
+        from("jms:queue:sms")
+                .throttle(2)
+                .timePeriodMillis(1000)
+                .bean("przemoConnector", "send(${body}");
+        from("jms:queue:sms")
+                .throttle(2)
+                .timePeriodMillis(1000)
+                .bean("wojtekConnector", "send(${body}");
     }
 }
