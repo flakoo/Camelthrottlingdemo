855daedc61ac5f4e5eb7c3f18e470056f6272e0d
Jan Falkowski <falkowski.jj@gmail.com>
2016-07-13 22:49:40 +0200

    Non-throttled error handler

--- src/main/java/org/alpacology/SmsConnector.java
+++ src/main/java/org/alpacology/SmsConnector.java
@@ -4,14 +4,22 @@ import com.sun.istack.internal.logging.Logger;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class SmsConnector implements BeanNameAware{
     Logger LOGGER = Logger.getLogger(SmsConnector.class);
 
     private String name;
 
-    public void send(String sms) {
-        LOGGER.info(name + " is sending SMS: " + sms);
+    public void send(String sms) throws Exception {
+        Random random = new Random();
+        if (random.nextBoolean() == true) {
+            LOGGER.info(name + " is sending SMS: " + sms);
+        } else {
+            LOGGER.warning(name + " failed sending SMS: " + sms);
+            throw new Exception();
+        }
     }
 
     @Override
--- src/main/java/org/alpacology/camel/SmsRouter.java
+++ src/main/java/org/alpacology/camel/SmsRouter.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component;
 public class SmsRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
+        errorHandler(deadLetterChannel("jms:queue:dead")
+                .maximumRedeliveries(3)
+                .redeliveryDelay(1000));
+
         from("jms:queue:sms")
                 .throttle(2)
                 .timePeriodMillis(1000)
