d246e2349cee18130f26a8714e970ed6ed92da58
Jan Falkowski <falkowski.jj@gmail.com>
2016-12-15 19:11:55 +0100

    Try-catch syntax

--- src/main/java/org/alpacology/camel/SmsRouter.java
+++ src/main/java/org/alpacology/camel/SmsRouter.java
@@ -1,9 +1,12 @@
 package org.alpacology.camel;
 
+import org.alpacology.connector.ConnectorException;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class SmsRouter extends RouteBuilder {
@@ -13,22 +16,32 @@ public class SmsRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        onException(Exception.class)
-                .handled(true)
-                .onWhen(resendPrediacte)
-                    .to("jms:queue:sms");
-        onException(Exception.class)
-                .handled(true)
-                .log(LoggingLevel.ERROR, "GIVING UP :(");
-
         from("jms:queue:sms")
-                .throttle(2)
-                .timePeriodMillis(1000)
-                .bean("inputOutputExceptionSmsConnector", "send(${body}");
+                .doTry()
+                    .throttle(2)
+                    .timePeriodMillis(1000)
+                    .bean("inputOutputExceptionSmsConnector", "send(${body}")
+                .endDoTry()
+                .doCatch(IOException.class)
+                    .handled(true)
+                        .onWhen(resendPrediacte)
+                    .to("jms:queue:sms")
+                .doCatch(IOException.class)
+                    .handled(true)
+                    .log(LoggingLevel.ERROR, "GIVING UP :(");
         from("jms:queue:sms")
-                .throttle(2)
-                .timePeriodMillis(1000)
-                .bean("connectorExceptionSmsConnector", "send(${body}");
+                .doTry()
+                    .throttle(2)
+                    .timePeriodMillis(1000)
+                    .bean("connectorExceptionSmsConnector", "send(${body}")
+                .endDoTry()
+                .doCatch(ConnectorException.class)
+                    .handled(true)
+                    .onWhen(resendPrediacte)
+                        .to("jms:queue:sms")
+                .doCatch(ConnectorException.class)
+                    .handled(true)
+                    .log(LoggingLevel.ERROR, "GIVING UP :(");
         from("jms:queue:sms")
                 .throttle(2)
                 .timePeriodMillis(1000)
